6794a1ad89cfff0d27133c158d0e41f35fcf40be
Mathies Schou <au587194@uni.au.dk>
2023-11-27 17:47:26 +0100

    StateManagement tilføjet.

--- build/CMakeFiles/MainProgram.dir/cmake_clean.cmake
+++ build/CMakeFiles/MainProgram.dir/cmake_clean.cmake
@@ -3,6 +3,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/MainProgram.dir/main.cpp.o.d"
   "CMakeFiles/MainProgram.dir/sourcefiles/LogIn.cpp.o"
   "CMakeFiles/MainProgram.dir/sourcefiles/LogIn.cpp.o.d"
+  "CMakeFiles/MainProgram.dir/sourcefiles/StateManagement.cpp.o"
+  "CMakeFiles/MainProgram.dir/sourcefiles/StateManagement.cpp.o.d"
   "MainProgram"
   "MainProgram.pdb"
 )
---
+++ build/CMakeFiles/stateManagement.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for stateManagement.
+# This may be replaced when dependencies are built.
