dc2f238df76dd5b74fa7c727d905f92e173b8e53
mathiesschou <mathiesschou@icloud.com>
2024-01-09 19:14:43 +0100

    h

--- AlarmSystem/IntrusionDetector.h
+++ AlarmSystem/IntrusionDetector.h
@@ -11,14 +11,10 @@ public:
     ~IntrusionDetector();
 
     bool checkForIntrusion(const InputData& InputData);
-
-    void measure(int *result, int sensorData[2], int cameraData[9][9]);
-
     void handleIntrusion(StateManagement& stateManager, LogIn& loginSystem);
+    bool isIntrusionDetected(int sensorSum, int cameraData[9][9]);
 
-    bool isIntrusionDetected(int sensorSum, int cameraData[9][9]); // Updated method signature
 private:
-    
     bool alarmTriggered;
 };
 
